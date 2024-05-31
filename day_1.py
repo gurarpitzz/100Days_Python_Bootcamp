d1d7373570c535c41b3a770ec6af2e208485cd40
= <=>
2024-05-31 19:53:07 +0530

    New Commit

--- day_1.py
+++ day_1.py
@@ -1,1 +1,5 @@
-print("hello world")
+print("welcome to the band name generator")
+a= input("Whats the name of the city you grew up in?\n")
+b= input("what is the name of your pet?\n")
+c=a+" "+b
+print(f"your band name could be {c}")
